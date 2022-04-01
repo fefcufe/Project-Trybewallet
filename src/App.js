85bacbc2981e0166632979e2680b0b35f5d7fd9b
Fernanda Ferreira <fefcufe@gmail.com>
2022-03-31 21:10:59 -0300

    Requisito 1 em processo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Wallet from './pages/Wallet';
+import Login from './pages/Login';
 
 function App() {
-  return <div>Hello, TrybeWallet!</div>;
+  return (
+    <div>
+      <nav id="nav-bar" />
+      <h1> Hello, TrybeWallet! </h1>
+
+      <Switch>
+        <Route path="/carteira" component={ Wallet } />
+        <Route path="/" component={ Login } />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,92 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: '',
+      isButtonDisabled: true,
+    };
+
+    this.onEmailChange = this.onEmailChange.bind(this);
+    this.onPasswordChange = this.onPasswordChange.bind(this);
+    this.validateLoginButton = this.validateLoginButton.bind(this);
+  }
+
+  onEmailChange({ target }) {
+    const { value } = target;
+    this.setState({
+      email: value,
+    }, this.validateLoginButton);
+  }
+
+  onPasswordChange({ target }) {
+    const { value } = target;
+    this.setState({
+      password: value,
+    }, this.validateLoginButton);
+  }
+
+  validateLoginButton() {
+    // Regular expression obtida de https://stackoverflow.com/questions/46155/whats-the-best-way-to-validate-an-email-address-in-javascript
+
+    const { email, password } = this.state;
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const emailValidation = emailRegex.test(email);
+    const maxPasswordLength = 6;
+    const passwordValidation = password.length >= maxPasswordLength;
+    if (emailValidation && passwordValidation) {
+      this.setState({
+        isButtonDisabled: false,
+      });
+    }
+  }
+
   render() {
-    return <div>Login</div>;
+    const { email, password, isButtonDisabled } = this.state;
+    const handleClick = () => {
+      const { history } = this.props;
+      history.push('/carteira');
+    };
+
+    return (
+
+      <div className="login">
+        <h3>Login</h3>
+        <input
+          data-testid="email-input"
+          placeholder="email"
+          onChange={ this.onEmailChange }
+          value={ email }
+          type="email"
+        />
+
+        <input
+          data-testid="password-input"
+          placeholder="password"
+          onChange={ this.onPasswordChange }
+          value={ password }
+          type="password"
+        />
+        <button
+          type="button"
+          onClick={ this.handleClick }
+          disabled={ isButtonDisabled }
+        >
+          Entrar
+        </button>
+
+      </div>
+    );
   }
 }
+
+Login.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+};
 
 export default Login;
